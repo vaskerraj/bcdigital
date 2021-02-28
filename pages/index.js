d6cb098afbdaf9a218875404554c67cc6b4c7b93
vaskerraj <yohungdha@gmail.com>
2021-02-28 22:23:19 +0545

    fixing react fragment

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous"></link>
       </Head>
-    </div>
+    </>
   )
 }
 
