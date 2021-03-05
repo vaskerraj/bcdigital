9955ca90e35cab52b7fd52922c23d2dea8d0a114
vaskerraj <yohungdha@gmail.com>
2021-03-05 14:36:08 +0545

    process.env have problem

--- firebase/firebaseClient.js
+++ firebase/firebaseClient.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSSAGING_SENDERID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyD1jF9BDvaxZg8e29K_z8sG_iU5q-z2hB4",
+    authDomain: "bcdigital-auth.firebaseapp.com",
+    projectId: "bcdigital-auth",
+    storageBucket: "bcdigital-auth.appspot.com",
+    messagingSenderId: "424777676703",
+    appId: "1:424777676703:web:1ad148ac35ae07b2522faa"
 };
 
 // Initialize Firebase
