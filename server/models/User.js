f66a4dbb2c004e6c2a8bf0af5e2d22f700c00570
vaskerraj <yohungdha@gmail.com>
2021-03-05 13:03:08 +0545

    set mobile unique

--- server/models/User.js
+++ server/models/User.js
@@ -6,10 +6,10 @@ const userSchema = new mongoose.Schema({
     name: String,
     username: {
         type: String,
-        unique: true,
     },
     mobile: {
         type: String,
+        unique: true,
         required: true,
         index: true
     },
