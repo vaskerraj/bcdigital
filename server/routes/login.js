57d8bebc223b5e871047b1c2599d3a436c709162
vaskerraj <yohungdha@gmail.com>
2021-03-05 13:01:32 +0545

    create token & send to client using firebase auth

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('Users');
 
+const admin = require('../../firebase/firebaseAdmin');
+
 module.exports = function (server) {
 
     server.post('/api/login', async (req, res) => {
@@ -16,10 +18,23 @@ module.exports = function (server) {
 
         try {
             await user.comparePassword(password);
+
+            // using firebase custom auth using
             // create token and send to client
 
-            //sample return
-            return res.status(200).json(user);
+            // use objectid of database uid
+            const uid = user._id.toString();
+
+            await admin.auth().createCustomToken(uid)
+                .then(function (token) {
+                    return res.json({
+                        token
+                    });
+                })
+                .catch(function (error) {
+                    return res.status(500).json({ error: "Error during token creation" });
+                });
+
         } catch (err) {
             return res.status(422).json({ error: 'Invalid email or password.' });
         }
