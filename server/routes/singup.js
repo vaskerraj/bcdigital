c76b03caf31284544aa496229d38913ce247fe94
vaskerraj <yohungdha@gmail.com>
2021-03-05 11:38:20 +0545

    add custom auth information to database

--- server/routes/singup.js
+++ server/routes/singup.js
@@ -1,9 +1,18 @@
+const mongoose = require('mongoose');
+const Users = mongoose.model('Users');
 module.exports = function (server) {
 
-    server.post('/api/signup', (req, res) => {
-        res.status(200).json({
-            msg: 'message at login page'
-        })
+    server.post('/api/signup', async (req, res) => {
+        const { mobile, password } = req.body;
+        try {
+            const user = new Users({ mobile, password });
+            await user.save();
+
+            return res.status(200).json({ msg: 'user created' });
+
+        } catch (error) {
+            return res.status(422).json({ msg: error.messsage });
+        }
     });
 
 };
