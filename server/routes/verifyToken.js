9126bf7ac6c5b8cab13bf96374f17ded7f440f81
vaskerraj <yohungdha@gmail.com>
2021-03-07 15:12:15 +0545

    send user and token info to client as auth method

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -6,7 +6,36 @@ const admin = require('../../firebase/firebaseAdmin');
 module.exports = function (server) {
 
     server.post('/api/verifyToken', async (req, res) => {
-        const firebaseUser = await admin.auth().verifyIdToken(req.headers.token);
-        console.log(firebaseUser);
+        try {
+            const firebaseUser = await admin.auth().verifyIdToken(req.headers.token);
+            console.log(firebaseUser);
+            const { name, email, firebase: { sign_in_provider }, uid } = firebaseUser;
+            // on custom auth(phone number and password)
+            if (sign_in_provider === 'custom') {
+                const user = await User.findById(uid);
+                return res.status(200).json({
+                    user: {
+                        name: user.name,
+                        role: user.role,
+                        method: user.method
+                    },
+                    token: req.headers.token
+                });
+            }
+
+            // for social auth
+            return res.status(200).json({
+                user: {
+                    name,
+                    role: 'subscriber', // social auth always a 'subscriber'
+                    method: sign_in_provider
+                },
+                token: req.headers.token
+            });
+        } catch (error) {
+            res.status(401).json({
+                err: "Invalid or expired token",
+            });
+        }
     });
 };
