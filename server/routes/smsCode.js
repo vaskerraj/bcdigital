f8c5aec312c3622cd941b386b8535a35fe3c0044
vaskerraj <yohungdha@gmail.com>
2021-03-18 12:52:03 +0545

    check user exisit !

--- server/routes/smsCode.js
+++ server/routes/smsCode.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const smsGetway = require('../utils/sms');
 const SMS = mongoose.model('Sms');
+const Users = mongoose.model('Users');
 
 module.exports = function (server) {
 
@@ -8,7 +9,13 @@ module.exports = function (server) {
         const { mobile, method } = req.body;
         const code = Math.floor(100000 + Math.random() * 900000);
         try {
-
+            // reduce unnecessary sms if user is already registered with mobile number already
+            if (method === 'registration') {
+                const user = await Users.findOne({ username: mobile, method: 'custom', role: 'subscriber' });
+                if (user) {
+                    return res.status(422).json({ error: 'User alerady exists with this mobile number' });
+                }
+            }
             const sms = new SMS({ mobile, code, method, staus: 'active' });
             await sms.save();
 
