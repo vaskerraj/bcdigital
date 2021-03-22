f0c9047771cb54eb7bc8bc8fdbbb17fd5e092d10
vaskerraj <yohungdha@gmail.com>
2021-03-22 21:52:44 +0545

    change password api at common api file

---
+++ server/routes/common.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Users = mongoose.model('Users');
+
+const admin = require('../../firebase/firebaseAdmin');
+const { requiredAuth, checkRole } = require('../middlewares/auth');
+
+module.exports = function (server) {
+    server.post('/api/changePwd', requiredAuth, checkRole(['subscriber']), async (req, res) => {
+        const { current, password, method, role } = req.body;
+        try {
+            const user = await Users.findOne({ _id: req.user._id, method, role });
+            const comparePwd = await user.comparePassword(current);
+
+            await Users.findByIdAndUpdate({ _id: req.user._id }, { password }, {
+                new: true
+            });
+            return res.status(200).json({ msg: "success" });
+        } catch (error) {
+            return res.status(422).json({ error: 'Current password doesnt match.' });
+        }
+    });
+}
