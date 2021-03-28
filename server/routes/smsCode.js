2d87ed74f00c7be595092fecd74d82a71135b9d0
vaskerraj <yohungdha@gmail.com>
2021-03-28 21:06:52 +0545

    customize sms api as global to handle sending all type of sms

--- server/routes/smsCode.js
+++ server/routes/smsCode.js
@@ -8,6 +8,7 @@ module.exports = function (server) {
     server.post('/api/smscode', async (req, res) => {
         const { mobile, method } = req.body;
         const code = Math.floor(100000 + Math.random() * 900000);
+        var smsText = '';
         try {
             // reduce unnecessary sms if user is already registered with mobile number already
             if (method === 'registration') {
@@ -15,11 +16,18 @@ module.exports = function (server) {
                 if (user) {
                     return res.status(422).json({ error: 'User alerady exists with this mobile number' });
                 }
+                smsText = process.env.SMS_REGISTER_TEMP
+            } else if (method === 'password_recover') {
+                const user = await Users.findOne({ username: mobile, method: 'custom' });
+                if (!user) {
+                    return res.status(422).json({ error: 'User not exists with this mobile number' });
+                }
+                smsText = process.env.SMS_PASS_RECOVER_TEMP
             }
             const sms = new SMS({ mobile, code, method, staus: 'active' });
             await sms.save();
 
-            const registerSMSText = code + process.env.SMS_REGISTER_TEMP
+            const registerSMSText = code + smsText;
             const { msg } = await smsGetway(mobile, registerSMSText);
             if (msg === 'done') {
                 return res.status(200).json(sms);
@@ -28,9 +36,7 @@ module.exports = function (server) {
             }
 
         } catch (error) {
-            return res.status(422).json({
-                error: error.message
-            });
+            return res.status(422).json({ error: "Some error. Please try again later" });
         }
     });
 };
