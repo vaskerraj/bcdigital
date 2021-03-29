c70944bdd186f0834fecee1cbc1e8c97c7e86bee
vaskerraj <yohungdha@gmail.com>
2021-03-29 08:42:34 +0545

    recover password api

--- server/routes/common.js
+++ server/routes/common.js
@@ -3,6 +3,7 @@ const Users = mongoose.model('Users');
 
 const admin = require('../../firebase/firebaseAdmin');
 const { requiredAuth, checkRole } = require('../middlewares/auth');
+const verifySms = require('../utils/verifySms');
 
 module.exports = function (server) {
     server.post('/api/changePwd', requiredAuth, checkRole(['subscriber']), async (req, res) => {
@@ -17,6 +18,26 @@ module.exports = function (server) {
             return res.status(200).json({ msg: "success" });
         } catch (error) {
             return res.status(422).json({ error: 'Current password doesnt match.' });
+        }
+    });
+
+    server.post('/api/recoverPassword', async (req, res) => {
+        const { mobile, verificationCode, password, smsmethod, role } = req.body;
+        try {
+            // Note: checked mobile number at while sending sms so not check mobile number as username
+            const { msg } = await verifySms(mobile, verificationCode, smsmethod);
+            if (msg !== 'verify') {
+                return res.status(422).json({ error: 'Invalid SMS Verification Code' });
+            }
+
+            const user = await Users.findOne({ username: mobile, method: 'custom', role });
+            console.log(user);
+            user.password = password;
+            await user.save();
+
+            return res.status(200).json({ msg: "success" });
+        } catch (error) {
+            return res.status(422).json({ error: "Some error occur. Please try again later." });
         }
     });
 }
