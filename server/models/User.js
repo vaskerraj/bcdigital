977760d6f8e4c5d60aa514c48a4d710a2c2eafd4
vaskerraj <yohungdha@gmail.com>
2021-03-29 11:40:35 +0545

    modify User modal as adminRole needed

--- server/models/User.js
+++ server/models/User.js
@@ -41,6 +41,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: 'subscriber'
     },
+    adminRole: {
+        type: String
+    },
     cart: {
         type: Array,
         default: []
