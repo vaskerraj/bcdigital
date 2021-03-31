27dc74c8ec10f8d323522259446c59e844963c3a
vaskerraj <yohungdha@gmail.com>
2021-04-01 00:29:10 +0545

    implememting modal to add and edit categories

--- pages/admin/categories.js
+++ pages/admin/categories.js
@@ -5,20 +5,54 @@ import { useDispatch, useSelector } from 'react-redux';
 import { parseCookies } from 'nookies';
 import axios from 'axios';
 
+import axiosApi from '../../helpers/api';
+import { useForm } from 'react-hook-form';
+import { message } from 'antd';
+
 import Wrapper from '../../components/admin/Wrapper';
 import CategoryBlock from '../../components/admin/CategoryBlock';
+import CategoryModal from '../../components/admin/CategoryModal';
 import SubCategoryBlock from '../../components/admin/SubCategoryBlock';
+
+// config antdesign message
+message.config({
+    top: '19vh',
+    maxCount: 1,
+    duration: 3,
+});
 
 const Categories = ({ categories }) => {
     const [subCategories, setSubCategories] = useState('');
+    const [givenCategory, setGivenCategory] = useState('');
     const [activeCat, setActiveCat] = useState();
+
+    const [modalTitle, setModalTitle] = useState('');
+    const [visible, setVisible] = useState(false);
+
+    const router = useRouter();
+
+    const initInputValue = {
+        name: categories.name,
+        parentId: categories.parentId
+    }
+    const { register, handleSubmit, errors } = useForm({
+        defaultValues: initInputValue
+    });
 
     const dispatch = useDispatch();
 
     const { adminAuth } = useSelector(state => state.adminAuth);
 
     const addCategoryHandler = category => {
+        const categoryTitle = typeof (category) === 'string'
+            ?
+            `Add New Category`
+            :
+            `Add Sub-Category at ${category.name}`;
 
+        setModalTitle(categoryTitle);
+        setGivenCategory(category);
+        setVisible(true);
     }
 
     const editCategoriesHandler = (category, subCategory) => {
@@ -45,9 +79,55 @@ const Categories = ({ categories }) => {
             />
         );
     }
-
+    const onModalSubmit = async (inputdata) => {
+        try {
+            const { data } = await axiosApi.post('/api/categories', {
+                name: inputdata.name,
+                parentId: inputdata.parentId ? inputdata.parentId : null
+            }, {
+                headers: {
+                    token: adminAuth.token
+                }
+            });
+            if (data) {
+                setVisible(false);
+                message.success({
+                    content: (
+                        <div>
+                            <div className="font-weight-bold">Success</div>
+                            Category succssfully added
+                        </div>
+                    ),
+                    className: 'message-success',
+                });
+                setTimeout(() => {
+                    router.reload();
+                }, 2000);
+            }
+        } catch (error) {
+            setVisible(false);
+            message.warning({
+                content: (
+                    <div>
+                        <div className="font-weight-bold">Error</div>
+                        {error.response ? error.response.data.error : error.message}
+                    </div>
+                ),
+                className: 'message-warning',
+            });
+        }
+    }
     return (
         <Wrapper onActive="categories" breadcrumb={["Categories"]}>
+            <CategoryModal
+                title={modalTitle}
+                categoryArray={givenCategory}
+                visible={visible}
+                handleCancel={handleCancel}
+                formRegister={register}
+                handleSubmit={handleSubmit(onModalSubmit)}
+                errors={errors}
+            />
             <div className="d-block text-right mb-3">
                 {categories.length !== 0 &&
                     <Link href="/admin/sub-category">
