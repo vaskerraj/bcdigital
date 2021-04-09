37e3d502ebb2a1f700d45b79d77ba6c68b917be0
vaskerraj <yohungdha@gmail.com>
2021-04-09 18:16:51 +0545

    implement filter

--- pages/admin/banner/index.js
+++ pages/admin/banner/index.js
@@ -1,23 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import Image from 'next/image'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { parseCookies } from 'nookies';
 import axios from 'axios';
 
+import { useForm } from 'react-hook-form';
+
 import axiosApi from '../../../helpers/api';
-import { Popconfirm, message } from 'antd';
+import { Popconfirm, Select, message } from 'antd';
+const { Option, OptGroup } = Select;
+
 import { Edit3, Trash2 } from 'react-feather';
 
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 
 import Wrapper from '../../../components/admin/Wrapper';
+import ChooseCategory from '../../../components/ChooseCategory';
+import { allSellers } from '../../../redux/actions/sellerAction';
 
 const Brands = ({ banner }) => {
-    const [bannerItem, setBannerItem] = useState(Object.values(banner));
+    const [bannerItem, setBannerItem] = useState([]);
+
+    const [bannerPostionValue, setBannerPostionValue] = useState('');
+    const [fieldCategory, setFieldCategory] = useState(false);
+    const [fieldSellerList, setFieldSellerList] = useState(false);
+    // seller upload state
+    const [sellerList, setSellerList] = useState("");
+
+    const [onOpenChoosenCategory, setOnOpenChoosenCategory] = useState(false);
+    const [categoryId, setCategoryId] = useState('');
+    // get selected categories list and category id
+    const [confirmCategory, setConfirmCategory] = useState({
+        categoryId: null,
+        firstCatName: '',
+        secondCatName: '',
+        thirdCatName: ''
+    });
+
+    const [selectedCatText, setSelectedCatText] = useState('');
+
+    useEffect(() => {
+        const firstDivider = (confirmCategory.firstCatName && confirmCategory.secondCatName)
+            ? ' / '
+            : '';
+        const secondDivider = (confirmCategory.secondCatName && confirmCategory.thirdCatName)
+            ? ' / '
+            : '';
+        const selcatText = confirmCategory.firstCatName + firstDivider + confirmCategory.secondCatName + secondDivider + confirmCategory.thirdCatName;
+
+        setSelectedCatText(selcatText);
+
+        // categoryId state
+        setCategoryId(confirmCategory.categoryId);
+
+    }, [confirmCategory]);
+
+    const router = useRouter;
+    const dispatch = useDispatch();
 
     const { adminAuth } = useSelector(state => state.adminAuth);
+
+    // seller option
+    const { sellers } = useSelector(state => state.sellerList);
+    useEffect(async () => {
+        dispatch(allSellers());
+    }, [bannerPostionValue]);
+
+    //  bannerPostionHandler
+    const bannerPostionHandler = (value) => {
+        setBannerPostionValue(value);
+        switch (value) {
+            case 'position_home':
+                setFieldCategory(false);
+                // hide sellers list
+                setFieldSellerList(false);
+                break;
+            case 'position_seller':
+                setFieldCategory(false);
+                // display sellers list
+                setFieldSellerList(true);
+                break;
+            case 'position_category':
+                setFieldCategory(true);
+
+                // hide sellers list
+                setFieldSellerList(false);
+                break;
+            case " ":
+                setFieldCategory(false);
+                // hide sellers list
+                setFieldSellerList(false);
+            default:
+                setFieldCategory(false);
+                // hide sellers list
+                setFieldSellerList(false);
+                break;
+
+        }
+    }
+
+    const onChangeSeller = value => {
+        setSellerList(value);
+    }
+
+    const filterHandler = () => {
+        // bannerPostionValue  sellerList categoryId
+        let filteredBanner;
+        if (bannerPostionValue === "position_home") {
+            filteredBanner = banner.filter(bn => bn.bannerPosition === "position_home");
+        } else if (bannerPostionValue === "position_seller") {
+            filteredBanner = banner.filter(bn => bn.bannerPosition === "position_seller" && bn.sellerId === sellerList);
+        } else if (bannerPostionValue === "position_category") {
+            filteredBanner = banner.filter(bn => bn.bannerPosition === "position_category" && bn.categoryId === categoryId);
+        }
+        // need to convert to object for Sort and Reorder
+        setBannerItem(Object.values(filteredBanner ? filteredBanner : []));
+    }
 
     const onDeleteHandler = async (id) => {
         try {
@@ -36,9 +137,7 @@ const Brands = ({ banner }) => {
                     ),
                     className: 'message-success',
                 });
-                setTimeout(() => {
-                    router.reload();
-                }, 2000);
+                router.push(router.asPath);
             }
         } catch (error) {
             message.warning({
@@ -54,10 +153,10 @@ const Brands = ({ banner }) => {
     }
 
     const SortableBanners = SortableElement(({ value }) =>
-        <div className="col-sm-6 col-md-4 mt-4">
+        <div className="col-sm-6 col-md-6 col-lg-4 mt-4">
             <div className="brand-block d-block p-3 mt-3 mt-sm-0">
                 <div className="rounded img-thumbnail text-center">
-                    <Image src={`/uploads/brands/${value.image}`} height="160" width="100%" />
+                    <Image src={`/uploads/banners/${value.mobileImage}`} height="160" width="330" />
                 </div>
                 <div className="d-block mt-4 font-weight-bold font16">
                     {value.name}
@@ -116,7 +215,76 @@ const Brands = ({ banner }) => {
                     </button>
                 </Link>
             </div>
-            <div className="d-block">
+            <div className="row filter-container">
+                <div className="col-12 col-lg-11">
+                    <div className="row">
+                        <div className="col-12 col-md-5 mt-3">
+                            <label className="cat-label">Banner Position</label>
+                            <select defaultValue="" name="bannerPosition" className="form-control"
+                                onChange={(e) => bannerPostionHandler(e.target.value)}
+                            >
+                                <option value="">Select</option>
+                                <option value="position_home">Home</option>
+                                <option value="position_seller">Seller's Page</option>
+                                <option value="position_category">Category Page</option>
+                            </select>
+                        </div>
+                        {fieldCategory &&
+                            <div className="col-12 col-lg-7 mt-3 position-relative">
+                                <label className="cat-label">Cateogry</label>
+                                <input name="bannerCategory" className="form-control normal-input-readyonly"
+                                    readOnly
+                                    onClick={() => setOnOpenChoosenCategory(true)}
+                                    value={selectedCatText}
+                                    autoComplete="off"
+                                />
+                                {onOpenChoosenCategory &&
+                                    <div className="select-subcate-container pt-3 pr-3 pl-3 border">
+                                        <ChooseCategory
+                                            catLevel={2}
+                                            setConfirmCategory={setConfirmCategory}
+                                            handleCancel={setOnOpenChoosenCategory}
+                                        />
+                                    </div>
+                                }
+                            </div>
+                        }
+                        {
+                            fieldSellerList &&
+                            <div className="col-12 col-md-7 mt-3 ml-3 ml-sm-0">
+                                <label className="cat-label">Sellers</label>
+                                <Select defaultValue="" style={{ width: '100%', display: "block" }} onChange={onChangeSeller}>
+                                    <Option value="">Select</Option>
+                                    <OptGroup label="Own Shop">
+                                        {
+                                            sellers.filter(seller => seller.sellerRole === 'own').map(filteredSeller => (
+                                                <Option key={filteredSeller._id} value={filteredSeller._id}>{filteredSeller.name}/{filteredSeller.username}</Option>
+                                            ))
+                                        }
+                                    </OptGroup>
+                                    <OptGroup label="Sellers">
+                                        {
+                                            sellers.filter(seller => seller.sellerRole === 'normal').map(filteredSeller => (
+                                                <Option key={filteredSeller._id} value={filteredSeller._id}>{filteredSeller.name}/{filteredSeller.username}</Option>
+                                            ))
+                                        }
+                                    </OptGroup>
+                                </Select>
+                            </div>
+                        }
+                    </div>
+                </div>
+                <div className="col-12 col-lg-1 mt-4 ml-3 ml-sm-0">
+                    <button
+                        onClick={filterHandler}
+                        className="btn btn-lg btn-block btn-warning"
+                        style={{ marginTop: '2.3rem' }}
+                    >
+                        Filter
+                    </button>
+                </div>
+            </div>
+            <div className="d-block mt-3">
                 {(bannerItem && bannerItem.length === 0) && <div className="d-block text-center mt-5 text-muted font16">No Data</div>}
                 <SortableBrandList items={bannerItem} onSortEnd={onSortEnd} />
             </div>
