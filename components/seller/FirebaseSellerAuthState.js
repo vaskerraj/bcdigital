a92a0cce7cb342212bec371dc811b9ec86139e29
vaskerraj <yohungdha@gmail.com>
2021-04-27 18:17:59 +0545

    issue : token expire at each 1 hour as firebase docs fix: refresh token at each 10 min

--- components/seller/FirebaseSellerAuthState.js
+++ components/seller/FirebaseSellerAuthState.js
@@ -2,6 +2,9 @@
 import React, { useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import { setCookie, destroyCookie } from 'nookies';
+
+import axiosApi from '../../helpers/api';
+
 import firebase from "../../firebase/firebaseClient";
 import { SELLER_SIGIN_SUCCESS } from "../../redux/types/sellerAuthType";
 
@@ -13,7 +16,9 @@ const FirebaseSellerAuthState = ({ children }) => {
 
                 // cookies
                 destroyCookie(null, "sell_token");
-                setCookie(null, "sell_token", "", {});
+                setCookie(null, "sell_token", "", {
+                    path: '/'
+                });
 
                 // dispatch logout
                 await firebase.auth().signOut();
@@ -34,6 +39,38 @@ const FirebaseSellerAuthState = ({ children }) => {
                 dispatch({ type: SELLER_SIGIN_SUCCESS, payload: dispatchData })
             }
         });
+    }, []);
+
+    // force refresh the token every 10 minutes
+    useEffect(() => {
+        const handle = setInterval(async () => {
+            const user = firebase.auth().currentUser;
+            if (user) {
+                const token = await user.getIdToken(true);
+
+                // cookies
+                destroyCookie(null, "sell_token");
+                setCookie(null, "sell_token", "", {
+                    path: '/'
+                });
+
+                // get user info from backend, not firebase
+                const { data } = await axiosApi.get(`/api/verifyToken`, {
+                    headers: {
+                        token
+                    }
+                });
+                console.log("force refresh", data)
+                // dispatch login
+                const dispatchData = {
+                    user: data.user,
+                    token: data.token
+                }
+                dispatch({ type: SELLER_SIGIN_SUCCESS, payload: dispatchData })
+            }
+        }, 10 * 60 * 1000);
+
+        return () => clearInterval(handle);
     }, []);
 
     return <>{children}</>;
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -5,37 +5,37 @@ const admin = require('../../firebase/firebaseAdmin');
 
 module.exports = function (server) {
 
-    server.post('/api/verifyToken', async (req, res) => {
+    server.get('/api/verifytoken', async (req, res) => {
         try {
-            const firebaseUser = await admin.auth().verifyIdToken(req.headers.token);
-            console.log(firebaseUser);
-            const { name, email, firebase: { sign_in_provider }, uid } = firebaseUser;
+
+            const token = req.headers.token;
+            if (!token) return res.status(400).json({ error: 'Invalid Authentication' })
+
+            const firebaseUser = await admin.auth().verifyIdToken(token);
+
+            const { firebase: { sign_in_provider }, uid } = firebaseUser;
             // on custom auth(phone number and password)
             if (sign_in_provider === 'custom') {
                 const user = await User.findById(uid);
                 return res.status(200).json({
-                    user: {
-                        name: user.name,
-                        role: user.role,
-                        method: user.method
-                    },
-                    token: req.headers.token
+                    user: user.name,
+                    token
                 });
-            }
+            } else {
+                // Check if user exists with refresh token (socail auth)
+                const user = await User.findOne({ fireuid: firebaseUser.uid });
+                if (!user) {
+                    return res.status(401).json({ error: 'Invalid Authentication' });
+                }
 
-            // for social auth
-            return res.status(200).json({
-                user: {
-                    name,
-                    role: 'subscriber', // social auth always a 'subscriber'
-                    method: sign_in_provider
-                },
-                token: req.headers.token
-            });
+                return res.status(200).json({
+                    user: user.name,
+                    token
+                });
+
+            }
         } catch (error) {
-            res.status(401).json({
-                err: "Invalid or expired token",
-            });
+            res.status(401).json({ error: "Invalid or expired token" });
         }
     });
 };
