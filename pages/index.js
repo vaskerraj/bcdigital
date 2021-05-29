ec59bfb120dac9fc553d5344b7552850fc6f9109
vaskerraj <yohungdha@gmail.com>
2021-05-29 21:27:53 +0545

    services part with styling

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,14 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import axios from 'axios';
 
+import { Phone, RotateCcw, Shield, Truck } from 'react-feather';
+import { HistoryOutlined } from '@ant-design/icons'
+
 import { listProducts } from '../redux/actions/productListAction';
 import ImageCarousel from '../components/ImageCarousel';
 import LastestProductSlider from '../components/helpers/LastestProductSlider';
 
-const numberOfLatestProduct = 10;
+const numberOfLatestProduct = 20;
 const numberOfTrendingProduct = 10;
 
 const Home = ({ banners, products }) => {
@@ -34,8 +37,8 @@ const Home = ({ banners, products }) => {
           data={banners}
           autoplay={true}
           autoplaySpeed={5000}
-          imgWidth={'1250px'}
-          imgHeight={'410px'}
+          imgWidth={'1188px'}
+          imgHeight={'350px'}
           imgQuality={60}
         />
         <div className="container mt-5">
@@ -45,9 +48,51 @@ const Home = ({ banners, products }) => {
           <div className="d-block slide">
             <LastestProductSlider data={products} />
           </div>
+          <div className="col mt-5">
+            <div className="row services bg-white border pt-4 pb-4">
+              <div className="col-sm-3 mt-2 mt-sm-0">
+                <div className="d-flex">
+                  <Shield size={38} color="#eb1c25" className="icon mr-1" />
+                  <div>
+                    <b>100% Secured Payments</b>
+                    <div className="d-block">Your Payment Are Safe With Us.</div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-3 mt-2 mt-sm-0 border-left">
+                <div className="d-flex">
+                  <RotateCcw size={38} className="icon mr-1 text-primary" />
+                  <div>
+                    <b>Free Returns</b>
+                    <div className="d-block">9 Days To Change Your Mind</div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-3 mt-2 mt-sm-0 border-left">
+                <div className="d-flex">
+                  <Phone size={38} className="icon mr-1 text-success" />
+                  <div>
+                    <b>Support 24/7</b>
+                    <div className="d-block">
+                      Contact Us 24 hours A Day
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-3 mt-2 mt-sm-0 border-left">
+                <div className="d-flex">
+                  <Truck size={38} className="icon mr-1 text-info" />
+                  <div>
+                    <b>Home Delivery</b>
+                    <div className="d-block">Home Delivery To Your City</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   )
 }
 export async function getServerSideProps(context) {
