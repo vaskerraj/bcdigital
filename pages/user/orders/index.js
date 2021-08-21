b59e9ab369a321be6dac7ee7a38e311043238cc8
vaskerraj <yohungdha@gmail.com>
2021-08-21 22:12:30 +0545

    modify UI for mobile and big screens with back icon to handle navigation

--- pages/user/orders/index.js
+++ pages/user/orders/index.js
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { ArrowLeft } from 'react-feather';
 import { parseCookies } from 'nookies';
 import { useSelector } from 'react-redux';
 
 import axios from 'axios';
 import axiosApi from '../../../helpers/api';
 
-import { Layout, Card, message, Spin } from 'antd';
+import { Layout, Card, message, Spin, Affix } from 'antd';
 const { Content } = Layout;
 import { LoadingOutlined } from '@ant-design/icons';
 
 import moment from 'moment';
 
+import useWindowDimensions from '../../../helpers/useWindowDimensions';
 import UserSidebarNav from '../../../components/nav/UserSidebarNav';
 import Wrapper from '../../../components/Wrapper';
 
 const Orders = ({ orders }) => {
 
+    const { width } = useWindowDimensions();
+    const [onlyMobile, setOnlyMoble] = useState(false);
+
+    useEffect(() => {
+        if (width <= 576) {
+            setOnlyMoble(true);
+        } else {
+            setOnlyMoble(false);
+        }
+    }, [width])
+
     const [loadingOrderItems, setLoadingOrderItems] = useState(false);
     const [orderItems, setOrderItems] = useState(orders);
     const [orderFilter, setOrderFilter] = useState(2);
 
+    const router = useRouter();
     const { userInfo } = useSelector(state => state.userAuth);
 
     const customLoadingIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
@@ -60,13 +75,25 @@ const Orders = ({ orders }) => {
                 <title>Orders | BC Digital</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="container mt-5">
+            {onlyMobile &&
+                <Affix offsetTop={70}>
+                    <div className="row bg-white backNav-container border-top p-2">
+                        <div className="d-flex align-items-center mb-2">
+                            <ArrowLeft className="mr-3" onClick={() => router.back()} />
+                            My Orders
+                        </div>
+                    </div>
+                </Affix>
+            }
+            <div className="container mt-4">
                 <Layout>
-                    <UserSidebarNav onActive="orders" />
+                    {!onlyMobile &&
+                        <UserSidebarNav onActive="orders" />
+                    }
                     <Layout className="site-layout">
                         <Content
                             style={{
-                                margin: '0 0 0 15px'
+                                margin: onlyMobile ? '0' : '0 0 0 15px'
                             }}>
                             <Card style={{
                                 minHeight: '60vh'
@@ -100,32 +127,67 @@ const Orders = ({ orders }) => {
                                                 className="d-flex title border-bottom justify-content-between p-3 pl-4 font13"
                                                 style={{ backgroundColor: '#fafafa' }}
                                             >
-                                                <div className="d-flex justify-content-around">
-                                                    <div className="d-block">
-                                                        <div className="text-uppercase font12 font-weight-bold" style={{ fontSize: '1.2rem' }}>Order Placed</div>
-                                                        <div>
-                                                            {moment(order.createdAt).format("DD MMMM YYYY")}
+                                                {!onlyMobile ?
+                                                    <>
+                                                        <div className="d-flex justify-content-around">
+                                                            <div className="d-block">
+                                                                <div className="text-uppercase font12 font-weight-bold" style={{ fontSize: '1.2rem' }}>Order Placed</div>
+                                                                <div>
+                                                                    {moment(order.createdAt).format("DD MMMM YYYY")}
+                                                                </div>
+                                                            </div>
+                                                            <div className="d-none d-sm-block ml-5">
+                                                                <div className="text-uppercase font12 font-weight-bold">Total</div>
+                                                                <div>Rs.{order.grandTotal}</div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="d-block text-right">
+                                                            <div className="text-uppercase">
+                                                                <span className="font12 font-weight-bold">Order</span> #{order._id}
+                                                            </div>
+                                                            <Link href={`/user/orders/${order._id}`}>
+                                                                <a className="text-info">View Details</a>
+                                                            </Link>
+                                                        </div>
+                                                    </>
+                                                    :
+                                                    <div style={{ width: '100%' }}>
+                                                        <div className="d-flex justify-content-between">
+                                                            <div>
+                                                                <div className="text-uppercase">
+                                                                    <span className="font12 font-weight-bold">Order</span> #{order._id}
+                                                                </div>
+                                                                <div className="d-block">
+                                                                    <span className="text-uppercase font12 font-weight-bold mr-1" style={{ fontSize: '1.2rem' }}>
+                                                                        Order Placed
+                                                                    </span>
+                                                                    {moment(order.createdAt).format("DD MMMM YYYY")}
+                                                                </div>
+                                                            </div>
+                                                            <div className="text-right">
+                                                                <div className="d-block">
+                                                                    <span className="text-uppercase font12 font-weight-bold mr-1" style={{ fontSize: '1.2rem' }}>
+                                                                        Total
+                                                                    </span>
+                                                                    Rs.{order.grandTotal}
+                                                                </div>
+                                                                <div className="d-block">
+                                                                    <Link href={`/user/orders/${order._id}`}>
+                                                                        <a className="text-info">View Details</a>
+                                                                    </Link>
+                                                                </div>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                    <div className="d-none d-sm-block ml-5">
-                                                        <div className="text-uppercase font12 font-weight-bold">Total</div>
-                                                        <div>Rs.{order.grandTotal}</div>
-                                                    </div>
-                                                </div>
-                                                <div className="d-block text-right">
-                                                    <div className="text-uppercase"><span className="font12 font-weight-bold">Order</span> #{order._id}</div>
-                                                    <Link href={`/ user / orders / ${order._id}`}>
-                                                        <a className="text-info">View Details</a>
-                                                    </Link>
-                                                </div>
+                                                }
                                             </div>
-                                            <div className="col-12 p-3">
+                                            <div className="col-12 p-0 p-md-3">
                                                 <ul className="list-unstyled">
                                                     {
                                                         order.products.map(item => (
                                                             <li key={item.products[0]._id} className={`cart-item item_${item.products[0]._id}`}>
                                                                 <div className="row">
-                                                                    <div className="col-6">
+                                                                    <div className="col-12 col-sm-8 col-md-6">
                                                                         <div className="d-flex">
                                                                             <Image src={`/uploads/products/${item.colour[0].images[0]}`}
                                                                                 layout="fixed"
@@ -135,18 +197,42 @@ const Orders = ({ orders }) => {
                                                                                 objectPosition="top center"
                                                                                 quality="50"
                                                                             />
-                                                                            <div className="product-detail ml-3">
+                                                                            <div className="product-detail ml-3" style={{ width: '100%' }}>
                                                                                 <div className="product-name">{item.name}</div>
-                                                                                <div className="mt-1">
-                                                                                    {item.products[0].size !== 'nosize' ? `Size : ${item.products[0].size} ` : ''}
+                                                                                <div className="d-flex justify-content-between align-items-center mt-1">
+                                                                                    <div>
+                                                                                        <div className="">
+                                                                                            {item.products[0].size !== 'nosize' ? `Size : ${item.products[0].size} ` : ''}
+                                                                                        </div>
+                                                                                        <div className="mt-1">
+                                                                                            <b>Qty</b>:{item.productQty}
+                                                                                        </div>
+                                                                                    </div>
+                                                                                    {onlyMobile &&
+                                                                                        <div>
+                                                                                            {item.orderStatus !== 'not_confirmed' && item.orderStatus !== 'confirmed'
+                                                                                                ?
+                                                                                                item.orderStatus === 'for_delivery' ?
+                                                                                                    <span className="badge bg-warning">On The Way</span>
+                                                                                                    :
+                                                                                                    <span className="badge bg-success text-capitalize">{item.orderStatus}</span>
+                                                                                                :
+                                                                                                <span className="badge bg-warning">Processing</span>
+                                                                                            }
+                                                                                        </div>
+                                                                                    }
                                                                                 </div>
-                                                                                <div className="mt-1">
-                                                                                    <b>Qty</b>:{item.productQty}
-                                                                                </div>
+                                                                                {onlyMobile &&
+
+                                                                                    <div className="d-block text-right mt-1">
+                                                                                        <button className="btn btn-warning btn-sm">Write a Review</button>
+                                                                                    </div>
+
+                                                                                }
                                                                             </div>
                                                                         </div>
                                                                     </div>
-                                                                    <div className="col-2 text-right">
+                                                                    <div className="d-none d-sm-block col-sm-4 col-md-2 text-right">
                                                                         {item.orderStatus !== 'not_confirmed' && item.orderStatus !== 'confirmed'
                                                                             ?
                                                                             item.orderStatus === 'for_delivery' ?
@@ -156,8 +242,11 @@ const Orders = ({ orders }) => {
                                                                             :
                                                                             <span className="badge bg-warning">Processing</span>
                                                                         }
+                                                                        <div className="d-none d-sm-block d-md-none d-lg-none mt-3">
+                                                                            <button className="btn btn-lg btn-warning">Write a Review</button>
+                                                                        </div>
                                                                     </div>
-                                                                    <div className="col-4 text-right">
+                                                                    <div className="d-none d-md-block col-md-4 text-right">
                                                                         {item.orderStatus === 'delivered' && item.paymentStatus === 'paid'
                                                                             ?
                                                                             <div className="d-block">
