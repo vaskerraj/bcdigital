657c6032fa0b5ebbf12b425f4d7481d8ed2ecef2
Dharma Raj Bhanari <yohungdha@gmail.com>
2022-04-13 08:43:22 +0545

    delete unnecessary div

--- components/nav/MainHeadSection.js
+++ components/nav/MainHeadSection.js
@@ -93,16 +93,6 @@ const MainHeadSection = ({ mobileTabBar }) => {
                         <div className={`${styles.topnav} row align-items-center`} >
                             <div className={` ${mobileTabBar !== "hide" ? 'col-12' : 'col-6'} col-sm-4 col-lg-3 order-first`}>
                                 <div className="d-flex">
-                                    <div className="mobileMenuToggle d-lg-none">
-                                        <div>
-                                            <svg viewBox="0 0 800 600">
-                                                <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
-                                                <path d="M300,320 L540,320" id="middle"></path>
-                                                <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318)">
-                                                </path>
-                                            </svg>
-                                        </div>
-                                    </div>
                                     <Link className="" href="/">
                                         <a>
                                             <img src="/logo192.png" height="53px" />
