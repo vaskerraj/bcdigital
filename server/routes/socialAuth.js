c753d39ad8427e6b9cda9fb22abc9120c87c93d4
Dharma Raj Bhandari <yohungdha@gmail.com>
2023-09-10 13:26:13 +1000

    socal auth API for mobile app

--- server/routes/socialAuth.js
+++ server/routes/socialAuth.js
@@ -28,4 +28,32 @@ module.exports = function (server) {
         }
     });
 
+    server.get('/api/app/social', async (req, res) => {
+        try {
+            // predefine password to prevent error(salt error on bcrypt)
+            const password = 'social';
+
+            const firebaseUser = await admin.auth().verifyIdToken(req.headers.token);
+            const { name, picture, email, phoneNumber, firebase: { sign_in_provider }, uid } = firebaseUser;
+
+            // check user is already registerd on database or not
+
+            const currentUser = await User.findOne({ fireuid: uid }).lean();
+            if (!currentUser) {
+                await new User({
+                    username: email ? email : phoneNumber,
+                    email,
+                    password,
+                    name,
+                    picture,
+                    fireuid: uid,
+                    method: sign_in_provider,
+                    registerMethod: 'app'
+                }).save();
+            }
+        } catch (error) {
+            return res.status(401).json({ error: 'Some error occur. Please try again later.' });
+        }
+    });
+
 };
